c7969a38e8237fb7ef6676f9d526b982991bd87f
leigh966 <leigh966@hotmail.com>
2022-11-15 16:21:48 +0000

    Now creates binary reps for each word that represent what letters it contains

--- WordleSolver.cpp
+++ WordleSolver.cpp
@@ -8,11 +8,41 @@
 #include <iomanip>      // std::setprecision
 #include <algorithm>
 #include <cctype>
+#include <format>
 using namespace std;
 
 const int MIN_CHAR_INT = 'a', MAX_CHAR_INT = 'z';
 
 list<string> fiveLetters;
+
+
+int getLetterIndex(char letter)
+{
+    
+    
+    // letters ordered by frequency in the oxford dictionary
+    //      source: http://letterfrequency.org/
+    const int NUM_LETTERS = 26;
+    char letters[NUM_LETTERS] = { 'e', 'a', 'r', 'i', 'o', 't','n', 's', 'l', 'c', 'u', 'd', 'p', 'm', 'h','g', 'b', 'f',
+        'y', 'w', 'k', 'v', 'x', 'z', 'j', 'q' };
+    for (int i = 0; i < NUM_LETTERS; i++)
+    {
+        if (letter == letters[i]) return i;
+    }
+    const string INVALID_LETTER_MESSAGE = (string)"Unrecognised character " + letter + " in word!";
+    throw(INVALID_LETTER_MESSAGE);
+}
+
+uint32_t binaryRep(string* s)
+{
+    uint32_t rep = 0;
+    for (auto it = s->begin(); it != s->end(); it++)
+    {
+        rep |= 1UL << getLetterIndex(*it);
+    }
+
+    return rep;
+}
 
 bool onlyAtoZ(string* s)
 {
@@ -47,6 +77,8 @@ void add(string myText)
     {
         fiveLetters.push_back(myText);
         cout << myText << endl;
+        cout << binaryRep(&myText) << endl;
+
     }
 }
 
@@ -65,6 +97,8 @@ void outputRuntime(time_t start, time_t end)
     cout << " sec " << endl;
 }
 
+
+
 int main()
 {
     time_t start, end;
@@ -75,10 +109,20 @@ int main()
 
     while (getline(MyFile, myText)) {
         toLower(&myText);
-        add(myText);
+        try {
+            add(myText);
+        }
+    
+    catch (string s)
+    {
+        cout << s << endl;
+        return -1;
+    }
     }
 
     time(&end);
+
+
 
     outputRuntime(start, end);
     return 0;
